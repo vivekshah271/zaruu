90013079bab62a1f3d45c5e389f35ac130361afe
vivekshah271 <89733293+vivekshah271@users.noreply.github.com>
2025-03-17 05:13:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('surpriseBtn').addEventListener('click', function() {
+    let message = document.getElementById('message');
+    message.classList.remove('hidden'); // Reveal the message
+});
